5a5fa51be8400256d0dedd83462c6b31870369b9
Matti Tahvonen <matti@vaadin.com>
2023-03-20 12:11:56 +0200

    V2 with enhanced memory savigs & easier usage

--- src/main/java/org/vaadin/addons/parttio/lightchart/LightChart.java
+++ src/main/java/org/vaadin/addons/parttio/lightchart/LightChart.java
@@ -1,13 +1,31 @@
 package org.vaadin.addons.parttio.lightchart;
 
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.charts.Chart;
 import com.vaadin.flow.component.charts.model.AbstractConfigurationObject;
 import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.Configuration;
 import com.vaadin.flow.component.charts.model.Series;
 import com.vaadin.flow.component.charts.util.ChartSerialization;
+import java.lang.reflect.Field;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- *
+ * This is a chart component that clears it potentially heavy Configuration
+ * after the chart has been drawn. This saves a considerable amount of memory in
+ * apps that both have a lot of users and use large charts.
+ * <p>
+ * The downside is that interactive features requiring server visits are not
+ * available. Also, updates to existing charts do not work.
+ * <p>
+ * Also, make sure you do not reference the Configuration from your own code.
+ * Otherwise this hack cannot free the configuration object for garbage 
+ * collection.
+ * 
  * @author mstahv
  */
 public class LightChart extends Chart {
@@ -19,9 +37,33 @@ public class LightChart extends Chart {
         super(type);
     }
 
+    @Override
+    public void drawChart(boolean b) {
+        super.drawChart(b);
+        freeMemory();
+    }
+
+    /**
+     * Not needed anymore as the the whole configuration object is cleared.
+     * 
+     * @param series s
+     * @deprecated
+     */
+    @Deprecated
     public void addSeriesWithoutLosingMemory(Series series) {
         String seriesStr = ChartSerialization.toJSON((AbstractConfigurationObject) series);
         getElement().executeJs("const s = JSON.parse($0); const el = this; setTimeout(()=>{el.__updateOrAddSeriesInstance(s);},10);", seriesStr);
+    }
+
+
+    private void freeMemory() {
+        try {
+            Field declaredField = getClass().getSuperclass().getDeclaredField("configuration");
+            declaredField.setAccessible(true);
+            declaredField.set(this, new Configuration());
+        } catch (NoSuchFieldException | SecurityException | IllegalAccessException ex) {
+            Logger.getLogger(LightChart.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
--- src/test/java/org/vaadin/addons/parttio/lightchart/AddonView.java
+++ src/test/java/org/vaadin/addons/parttio/lightchart/AddonView.java
@@ -1,14 +1,86 @@
 package org.vaadin.addons.parttio.lightchart;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.DataSeries;
+import com.vaadin.flow.component.charts.model.DataSeriesItem;
+import com.vaadin.flow.component.charts.model.Series;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.router.Route;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Random;
+import org.apache.commons.io.FileUtils;
 
 @Route("")
 public class AddonView extends Div {
 
+    Chart chart;
+
     public AddonView() {
-        LightChart theAddon = new LightChart();
-        theAddon.setId("theAddon");
-        add(theAddon);
+        RadioButtonGroup<Class<?>> group = new RadioButtonGroup<>();
+        group.setItemLabelGenerator(c -> c.getSimpleName());
+        group.setItems(LightChart.class, Chart.class);
+        group.addValueChangeListener(e -> {
+            Chart c;
+            if (e.getValue() == LightChart.class) {
+                c = new LightChart(ChartType.LINE);
+            } else {
+                c = new Chart();
+            }
+            c.getConfiguration().addSeries(createLargeSeries(c));
+            c.getConfiguration().addSeries(createLargeSeries(c));
+            c.getConfiguration().addSeries(createLargeSeries(c));
+            c.getConfiguration().addSeries(createLargeSeries(c));
+            add(c);
+
+            if (chart != null) {
+                remove(chart);
+            }
+
+            chart = c;
+        });
+
+        add(group);
+
+        Button button = new Button("Calculate UI size");
+        button.addClickListener(e -> {
+            String msg = serializeAndCalculateUISize();
+            Notification.show(msg).setDuration(10000);
+        });
+        add(button);
+        add(new Paragraph("Serialized UI size is not exactly what it consumes in JVM memory, because it is complicated. In reality it is more, ~ 2X, depending on data. But it is easy to calculated and it is probably well aligned relativile (when comparing LightChart & Chart)"));
+
+        group.setValue(LightChart.class);
+
+    }
+
+    private String serializeAndCalculateUISize() {
+        try {
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(bout);
+            objectOutputStream.writeObject(UI.getCurrent());
+            objectOutputStream.flush();
+            String msg = "Serialized UI size with " + chart.getClass().getSimpleName() + ":" + FileUtils.byteCountToDisplaySize(bout.size());
+            return msg;
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private Series createLargeSeries(Chart c) {
+        Random r = new Random(0);
+        DataSeries dataSeries = new DataSeries();
+        dataSeries.setName("Series " + c.getConfiguration().getSeries().size());
+        for (int i = 0; i < 100000; i++) {
+            dataSeries.add(new DataSeriesItem(i, r.nextInt(1000)));
+        }
+        return dataSeries;
     }
 }
